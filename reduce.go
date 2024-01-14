aae8e6e24ff301d9aa6e65aa12f3a6a5bb6f2e6b
cuishu <979962021@qq.com>
2024-01-14 22:21:21 +0800

    bug fix

--- reduce.go
+++ reduce.go
@@ -5,7 +5,7 @@ type ReduceFunc[A any] func(A, A) A
 func Reduce[A any](f ReduceFunc[A], list []A, init A) A {
 	var result A = init
 	for _, data := range list {
-		result = f(data, result)
+		result = f(result, data)
 	}
 	return result
 }
