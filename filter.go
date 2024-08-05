af16d3b6db3b0c8f62b60caac5b3f1bc13399acb
cuishu <979962021@qq.com>
2024-08-05 09:28:55 +0800

    Optimize filter performance

--- filter.go
+++ filter.go
@@ -3,7 +3,7 @@ package functools
 type FilterFunc[A any] func(A) bool
 
 func Filter[A any](f FilterFunc[A], list []A) []A {
-	var result []A
+	var result []A = make([]A, 0, len(list))
 	for _, data := range list {
 		if f(data) {
 			result = append(result, data)
